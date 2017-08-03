f05711090e3c3f94f5bc6910e61d1c87067d4339
kiwibatave <b_cuzon@yahoo.fr>
2017-08-03 16:40:28 +0200

    fourth

--- inscription.php
+++ inscription.php
@@ -9,15 +9,15 @@
     <div class="stepwizard-row setup-panel">
         <div class="stepwizard-step">
             <a href="#step-1" type="button" class="btn btn-primary btn-circle">1</a>
-            <p>Step 1</p>
+            <p>Etape 1</p>
         </div>
         <div class="stepwizard-step">
             <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
-            <p>Step 2</p>
+            <p>Etape 2</p>
         </div>
         <div class="stepwizard-step">
             <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
-            <p>Step 3</p>
+            <p>Etape 3</p>
         </div>
     </div>
 </div>
@@ -25,40 +25,40 @@
     <div class="row setup-content" id="step-1">
         <div class="col-xs-12">
             <div class="col-md-12">
-                <h3> Step 1</h3>
+                <h3> Etape 1</h3>
                 <div class="form-group">
-                    <label class="control-label">First Name</label>
+                    <label class="control-label">Prénom</label>
                     <input  maxlength="100" type="text" required="required" class="form-control" placeholder="Enter First Name"  />
                 </div>
                 <div class="form-group">
-                    <label class="control-label">Last Name</label>
+                    <label class="control-label">Nom</label>
                     <input maxlength="100" type="text" required="required" class="form-control" placeholder="Enter Last Name" />
                 </div>
-                <button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
+                <button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Suivant</button>
             </div>
         </div>
     </div>
     <div class="row setup-content" id="step-2">
         <div class="col-xs-12">
             <div class="col-md-12">
-                <h3> Step 2</h3>
+                <h3> Etape 2</h3>
                 <div class="form-group">
-                    <label class="control-label">Company Name</label>
+                    <label class="control-label">Mot de Passe</label>
                     <input maxlength="200" type="text" required="required" class="form-control" placeholder="Enter Company Name" />
                 </div>
                 <div class="form-group">
-                    <label class="control-label">Company Address</label>
+                    <label class="control-label">Confirmation du mot de passe </label>
                     <input maxlength="200" type="text" required="required" class="form-control" placeholder="Enter Company Address"  />
                 </div>
-                <button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
+                <button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Suivant</button>
             </div>
         </div>
     </div>
     <div class="row setup-content" id="step-3">
         <div class="col-xs-12">
             <div class="col-md-12">
-                <h3> Step 3</h3>
-                <button class="btn btn-success btn-lg pull-right" type="submit">Finish!</button>
+                <h3> Etape 3</h3>
+                <button class="btn btn-success btn-lg pull-right" type="submit">Terminé</button>
             </div>
         </div>
     </div>
